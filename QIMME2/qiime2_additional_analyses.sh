a879f8c1fd8eb7c17a97b8348c0c26937c7300fd
Mellaw <48302916+Mellaw@users.noreply.github.com>
2021-12-08 17:55:08 -0800

    Qiime2 scripts

---
+++ QIMME2/qiime2_additional_analyses.sh
@@ -0,0 +1,178 @@
+# To filter for infant observations 
+qiime feature-table filter-samples \
+  --i-table ./dada2_table.qza \
+  --m-metadata-file /data/t4_project2/infant/infant_metadata.txt \
+  --p-where "[life_stage]='Infant'" \
+  --o-filtered-table infant-filtered-table.qza
+
+#To filter for AD samples and visualize feature table
+qiime feature-table filter-samples \
+  --i-table ./infant-filtered-table.qza \
+  --m-metadata-file /data/t4_project2/infant/infant_metadata.txt \
+  --p-where "[eczema_inf]='yes'" \
+  --o-filtered-table /data/t4_project2/AD_outputs/AD-inf-filtered-table.qza
+
+qiime feature-table summarize \
+  --i-table ./AD-inf-filtered-table.qza \
+  --m-sample-metadata-file /data/t4_project2/infant/infant_metadata.txt \
+  --o-visualization ./AD-inf-filtered-table.qzv
+
+#to filter for non AD samples and visualize feature table
+qiime feature-table filter-samples \
+  --i-table ./infant-filtered-table.qza \
+  --m-metadata-file /data/t4_project2/infant/infant_metadata.txt \
+  --p-where "[eczema_inf]='no'" \
+  --o-filtered-table /data/t4_project2/Non_AD_outputs/Non-AD-inf-filtered-table.qza
+
+qiime feature-table summarize \
+  --i-table ./Non-AD-inf-filtered-table.qza \
+  --m-sample-metadata-file /data/t4_project2/infant/infant_metadata.txt \
+  --o-visualization ./Non-AD-inf-filtered-table.qzv
+
+# Generate alpha rarefaction curve for AD infants 
+qiime diversity alpha-rarefaction \  
+--i-table ./AD-inf-filtered-table.qza \
+--m-metadata-file /data/t4_project2/infant/infant_metadata.txt \  
+--o-visualization /data/t4_project2/AD_outputs/alpha_rarefaction_curves.qzv \   
+--p-min-depth 1 \  
+--p-max-depth 49972
+
+# Generate alpha rarefaction curve for Non- AD infants 
+qiime diversity alpha-rarefaction \  
+--i-table /data/t4_project2/Non_AD_outputs/Non-AD-inf-filtered-table.qza \
+--m-metadata-file /data/t4_project2/infant/infant_metadata.txt \  
+--o-visualization /data/t4_project2/Non-AD_outputs/alpha_rarefaction_curves.qzv \   
+--p-min-depth 1 \  
+--p-max-depth 49972
+
+# Generate phylogenetic tree
+qiime fragment-insertion sepp \
+  --i-representative-sequences ./dada2_rep_set.qza \
+  --i-reference-database /data/mouse_tutorial/sepp-refs-silva-128.qza\
+  --o-tree ./tree.qza \
+  --o-placements ./tree_placements.qza \
+  --p-threads 8
+
+# Diversity analysis (core metrics) for AD samples
+qiime diversity core-metrics-phylogenetic \
+  --i-table ./AD-inf-filtered-table.qza \
+  --i-phylogeny /data/t4_project2/script_outputs/tree.qza \
+  --m-metadata-file /data/t4_project2/infant/infant_metadata.txt \
+  --p-sampling-depth 26750 \
+  --output-dir ./core-metrics-results
+
+# Diversity analysis (core metrics) for non AD samples
+qiime diversity core-metrics-phylogenetic \
+  --i-table ./Non-AD-inf-filtered-table.qza \
+  --i-phylogeny /data/t4_project2/script_outputs/tree.qza \
+  --m-metadata-file /data/t4_project2/infant/infant_metadata.txt \
+  --p-sampling-depth 26750 \
+  --output-dir ./core-metrics-results
+
+# Generate faith_pd statistics for AD samples
+qiime diversity alpha-group-significance \
+  --i-alpha-diversity ./core-metrics-results/faith_pd_vector.qza \
+  --m-metadata-file /data/t4_project2/infant/infant_metadata.txt \
+  --o-visualization ./core-metrics-results/faiths_pd_statistics.qzv
+
+# Generate faith_pd statistics for non AD samples
+qiime diversity alpha-group-significance \
+  --i-alpha-diversity ./core-metrics-results/faith_pd_vector.qza \
+  --m-metadata-file /data/t4_project2/infant/infant_metadata.txt \
+  --o-visualization ./core-metrics-results/faiths_pd_statistics.qzv
+
+# Generate Pielou’s evenness statistics for AD samples
+qiime diversity alpha-group-significance \
+ --i-alpha-diversity ./core-metrics-results/evenness_vector.qza \
+ --m-metadata-file /data/t4_project2/infant/infant_metadata.txt \
+ --o-visualization ./core-metrics-results/evenness_statistics.qzv
+
+# Generate Pielou’s evenness statistics for non AD samples
+qiime diversity alpha-group-significance \
+ --i-alpha-diversity ./core-metrics-results/evenness_vector.qza \
+ --m-metadata-file /data/t4_project2/infant/infant_metadata.txt \
+ --o-visualization ./core-metrics-results/evenness_statistics.qzv
+
+# Beta Diversity for AD samples
+qiime diversity beta-group-significance \
+  --i-distance-matrix core-metrics-results/unweighted_unifrac_distance_matrix.qza \
+  --m-metadata-file /data/t4_project2/infant/infant_metadata.txt \
+  --m-metadata-column feed \
+  --o-visualization core-metrics-results/unweighted-unifrac-feed-significance.qzv
+
+qiime diversity beta-group-significance \
+  --i-distance-matrix core-metrics-results/weighted_unifrac_distance_matrix.qza \
+  --m-metadata-file /data/t4_project2/infant/infant_metadata.txt \
+  --m-metadata-column feed \
+  --o-visualization core-metrics-results/weighted-unifrac-feed-significance.qzv
+
+# Beta Diversity for non AD samples
+qiime diversity beta-group-significance \
+  --i-distance-matrix core-metrics-results/unweighted_unifrac_distance_matrix.qza \
+  --m-metadata-file /data/t4_project2/infant/infant_metadata.txt \
+  --m-metadata-column feed \
+  --o-visualization core-metrics-results/unweighted-unifrac-feed-significance.qzv
+
+qiime diversity beta-group-significance \
+  --i-distance-matrix core-metrics-results/weighted_unifrac_distance_matrix.qza \
+  --m-metadata-file /data/t4_project2/infant/infant_metadata.txt \
+  --m-metadata-column feed \
+  --o-visualization core-metrics-results/weighted-unifrac-feed-significance.qzv
+
+#Taxonomic classification for AD
+qiime feature-classifier classify-sklearn \
+  --i-reads /data/t4_project2/script_outputs/dada2_rep_set.qza \
+  --i-classifier /mnt/datasets/classifiers/silva-138-99-515-806-nb-classifier.qza  \
+  --o-classification ./taxonomy.qza
+
+#Taxonomic classification for non AD
+qiime feature-classifier classify-sklearn \
+  --i-reads /data/t4_project2/script_outputs/dada2_rep_set.qza \
+  --i-classifier /mnt/datasets/classifiers/silva-138-99-515-806-nb-classifier.qza  \
+  --o-classification ./taxonomy.qza
+
+#Tabulate taxonomy associated with the sequences with metadata for AD
+qiime metadata tabulate \
+  --m-input-file ./taxonomy.qza \
+  --o-visualization ./taxonomy.qzv
+
+#Tabulate taxonomy associated with the sequences with metadata for non AD
+qiime metadata tabulate \
+  --m-input-file ./taxonomy.qza \
+  --o-visualization ./taxonomy.qzv
+
+#Tabulate the representative sequence for AD
+qiime feature-table tabulate-seqs \
+  --i-data /data/t4_project2/script_outputs/dada2_rep_set.qza \
+  --o-visualization ./dada2_rep_set.qzv
+
+#Tabulate the representative sequence for non AD
+qiime feature-table tabulate-seqs \
+  --i-data /data/t4_project2/script_outputs/dada2_rep_set.qza \
+  --o-visualization ./dada2_rep_set.qzv
+
+#Filter out the sample with sequence fewer than the rarefaction depth set in the previous step (26750) for AD
+qiime feature-table filter-samples \
+  --i-table /data/t4_project2/script_outputs/infant-filtered-table.qza \
+  --p-min-frequency 26750 \
+  --o-filtered-table ./table_2k.qza
+
+#Filter out the sample with sequence fewer than the rarefaction depth set in the previous step (26750) for non AD
+qiime feature-table filter-samples \
+  --i-table /data/t4_project2/script_outputs/infant-filtered-table.qza \
+  --p-min-frequency 26750 \
+  --o-filtered-table ./table_2k.qza
+
+#Use rarefied table to build an interactive bar plot of taxonomy for AD samples
+qiime taxa barplot \
+  --i-table ./table_2k.qza \
+  --i-taxonomy ./taxonomy.qza \
+  --m-metadata-file /data/t4_project2/infant/infant_metadata.txt \
+  --o-visualization ./taxa_barplot.qzv
+
+#Use rarefied table to build an interactive bar plot of taxonomy for non AD samples
+qiime taxa barplot \
+  --i-table ./table_2k.qza \
+  --i-taxonomy ./taxonomy.qza \
+  --m-metadata-file /data/t4_project2/infant/infant_metadata.txt \
+  --o-visualization ./taxa_barplot.qzv
---
+++ QIMME2/qiime2_preprocessing_diversity.sh
@@ -0,0 +1,161 @@
+#Load data 
+#link relevant files to the team folder using symbolic links
+ln -s /mnt/datasets/project_2/infant
+
+#using manifest important data into qiime
+cd ~/data/t4_project2/infant
+
+qiime tools import \
+  --type "SampleData[SequencesWithQuality]" \
+  --input-format SingleEndFastqManifestPhred33V2 \
+  --input-path ./infant_manifest.txt \
+  --output-path /data/t4_project2/script_outputs/demux_seqs.qza
+ 
+#Conversion of qza to qzv
+cd ~/data/t4_project2/script_outputs
+
+qiime demux summarize \
+  --i-data ./demux_seqs.qza \
+  --o-visualization /data/t4_project2/script_outputs/demux_seqs.qzv
+
+#Quality control 
+#At truncation length 129, Phred for Q3 & Q4 = 37, 38
+qiime dada2 denoise-single \
+  --i-demultiplexed-seqs ./demux_seqs.qza \
+  --p-trunc-len 129 \
+  --o-table ./dada2_table.qza \
+  --o-representative-sequences ./dada2_rep_set.qza \
+  --o-denoising-stats ./dada2_stats.qza
+
+
+#FILTER FOR DESIRED METADATA CATEGORIES (check w Karen from her code)
+qiime feature-table filter-samples \
+  --i-table ./dada2_table.qza \
+  --m-metadata-file /data/t4_project2/infant/infant_metadata.txt \
+  --p-where "[life_stage]='Infant'" \
+  --o-filtered-table infant-filtered-table.qza
+
+#Review quality control step 
+qiime metadata tabulate \
+  --m-input-file ./dada2_stats.qza  \
+  --o-visualization ./dada2_stats.qzv
+  
+#Generate a features table 
+qiime feature-table summarize \
+  --i-table ./dada2_table.qza \
+  --m-sample-metadata-file /data/t4_project2/infant/infant_metadata.txt \
+  --o-visualization ./dada2_table.qzv
+
+#Download SILVA database and upload to server
+# URL; https://docs.qiime2.org/2021.8/data-resources/#sepp-reference-databases
+wget \
+  -O "sepp-refs-silva-128.qza" \
+  "https://docs.qiime2.org/2021.8/data-resources/#sepp-reference-databases"
+
+#Create session to run phylogenetic tree building in background 
+screen -S phylotree
+
+#Generate phylogentic trees
+qiime fragment-insertion sepp \
+  --i-representative-sequences ./dada2_rep_set.qza \
+  --i-reference-database sepp-refs-silva-128.qza\
+  --o-tree ./tree.qza \
+  --o-placements ./tree_placements.qza \
+  --p-threads 8  # update to a higher number if you can
+
+
+
+#Diversity Analysis 
+# Generate alpha rarefaction curve
+qiime diversity alpha-rarefaction \  
+--i-table /data/t4_project2/script_outputs/infant-filtered-table.qza \
+--m-metadata-file /data/t4_project2/infant/infant_metadata.txt \  
+--o-visualization /data/t4_project2/script_outputs/alpha_rarefaction_curves.qzv \   
+--p-min-depth 1 \  
+--p-max-depth 59159
+
+# Generate phylogenetic tree
+qiime fragment-insertion sepp \
+  --i-representative-sequences ./dada2_rep_set.qza \
+  --i-reference-database /data/mouse_tutorial/sepp-refs-silva-128.qza\
+  --o-tree ./tree.qza \
+  --o-placements ./tree_placements.qza \
+  --p-threads 8
+
+# Diversity analysis
+qiime diversity core-metrics-phylogenetic \
+  --i-table /data/t4_project2/script_outputs/infant-filtered-table.qza \
+  --i-phylogeny ./tree.qza \
+  --m-metadata-file /data/t4_project2/infant/infant_metadata.txt \
+  --p-sampling-depth 26745 \
+  --output-dir ./core-metrics-results
+
+# Generate faith_pd statistics
+qiime diversity alpha-group-significance \
+  --i-alpha-diversity ./core-metrics-results/faith_pd_vector.qza \
+  --m-metadata-file /data/t4_project2/infant/infant_metadata.txt \
+  --o-visualization ./core-metrics-results/faiths_pd_statistics.qzv
+
+# Generate Pielou’s evenness statistics
+qiime diversity alpha-group-significance \
+ --i-alpha-diversity ./core-metrics-results/evenness_vector.qza \
+ --m-metadata-file /data/t4_project2/infant/infant_metadata.txt \
+ --o-visualization ./core-metrics-results/evenness_statistics.qzv
+
+# Beta Diversity
+qiime diversity beta-group-significance \
+  --i-distance-matrix core-metrics-results/unweighted_unifrac_distance_matrix.qza \
+  --m-metadata-file /data/t4_project2/infant/infant_metadata.txt \
+  --m-metadata-column feed \
+  --o-visualization core-metrics-results/unweighted-unifrac-feed-significance.qzv
+
+qiime diversity beta-group-significance \
+  --i-distance-matrix core-metrics-results/weighted_unifrac_distance_matrix.qza \
+  --m-metadata-file /data/t4_project2/infant/infant_metadata.txt \
+  --m-metadata-column feed \
+  --o-visualization core-metrics-results/weighted-unifrac-feed-significance.qzv
+
+#Taxonomic classification and taxonomy barchart
+qiime feature-classifier classify-sklearn \
+  --i-reads ./dada2_rep_set.qza \
+  --i-classifier /mnt/datasets/classifiers/silva-138-99-515-806-nb-classifier.qza  \
+  --o-classification ./taxonomy.qza
+
+#Tabulate taxonomy associated with the sequences with metadata
+qiime metadata tabulate \
+  --m-input-file ./taxonomy.qza \
+  --o-visualization ./taxonomy.qzv
+
+#Tabulate the representative sequence
+qiime feature-table tabulate-seqs \
+  --i-data ./dada2_rep_set.qza \
+  --o-visualization ./dada2_rep_set.qzv
+
+#Filter out the sample with sequence fewer than the rarefaction depth set in the previous step (26745)
+qiime feature-table filter-samples \
+  --i-table /data/t4_project2/script_outputs/infant-filtered-table.qza \
+  --p-min-frequency 26745 \
+  --o-filtered-table ./table_2k.qza
+
+#Use rarefied table to build an interactive bar plot of taxonomy for each sample
+qiime taxa barplot \
+  --i-table ./table_2k.qza \
+  --i-taxonomy ./taxonomy.qza \
+  --m-metadata-file /data/t4_project2/infant/infant_metadata.txt \
+  --o-visualization ./taxa_barplot.qzv
+
+
+#Export infant filtered feature tables as .biom file
+qiime tools export \
+  --input-path ./infant-filtered-table.qza \
+  --output-path /root/data/t4_project2/script_outputs/qiime_out
+
+#Export taxonmic classification for each ASV as a tsv file
+qiime tools export \
+  --input-path ./taxonomy.qza \
+  --output-path /root/data/t4_project2/script_outputs/qiime_out
+
+#Export the rooted phylogenetic tree as a .nwk file
+qiime tools export \
+  --input-path ./tree.qza \
+  --output-path /root/data/t4_project2/script_outputs/qiime_out
